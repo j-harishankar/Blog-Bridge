09f51fb1b4dd11fdfa22dcf3fefcc186bdd12fbe
j harishankar <jharivichu27@gmail.com>
2025-08-03 12:03:31 +0530

    post created and dislayed - admin

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate,login,logout
+from .models import Post
 # Create your views here.
 
 
 def home(request):
-    return render(request,'blog/home.html')
+    context = {'posts':Post.objects.all()}
+    return render(request,'blog/home.html',context)
 def signup(request):
     return render(request, 'blog/signup.html')
 
