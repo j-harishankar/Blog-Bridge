6478be66bde5b56376529388997a5442bb085ae5
j harishankar <jharivichu27@gmail.com>
2025-08-03 23:14:23 +0530

    login and signup functionality completed

--- blog/views.py
+++ blog/views.py
@@ -9,11 +9,32 @@ from .models import Post
 def home(request):
     context = {'posts':Post.objects.all()}
     return render(request,'blog/home.html',context)
+
 def signup(request):
+    if request.method == 'POST':
+        name = request.POST.get('uname')
+        email= request.POST.get('uemail')
+        password = request.POST.get('upassword')
+        newUser = User.objects.create_user(username=name, email=email, password=password)
+        newUser.save()
+        return redirect('/loginn')
     return render(request, 'blog/signup.html')
 
 
 
+
 def loginn(request):
+    if request.method == 'POST':
+        name = request.POST.get('uname')
+        password = request.POST.get('upassword')
+        userr = authenticate(request, username=name, password=password)
+        if userr is not None:
+            login(request, userr)
+            return redirect('/home')
+        else:
+            return redirect('/login')
+
     return render(request, 'blog/login.html')
+
+
 
